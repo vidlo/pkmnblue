fc49ca785fc250c4f210852b6573f5f59de4922f
vidlo <ondrej338@gmail.com>
2018-04-08 19:22:28 +0200

    prvy commit

---
+++ du1Vyuka/src/main/java/com/github/vidlo/du1Vyuka/App.java
@@ -0,0 +1,13 @@
+package com.github.vidlo.du1Vyuka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
